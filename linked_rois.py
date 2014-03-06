bcfdb7a319a6bb66fc276594e5e13a64324a6e00
Luke Campagnola <luke.campagnola@gmail.com>
2014-03-05 22:28:57 -0500

    Introduced alternative method: Apply perspective transformation to the second ROI such that the local coordinate systems of both ROIs are identical.
    Added perspective to the transformation matrix to show off.

--- linked_rois.py
+++ linked_rois.py
@@ -80,18 +80,22 @@ image = image.T[:, ::-1]
 ###########################
 
 # Simulate another image with a perspective transformation relative to the
-# first image, by rotating it by 45° around its center.
+# first image, by rotating it by 45° around its center and adding a little 
+# perspective.
 
 # Rotate it by 45°...
+angle = math.pi / 4
 rotation = np.matrix([
-    [np.cos(math.pi / 4), np.sin(math.pi / 4), 0.0],
-    [-np.sin(math.pi / 4), np.cos(math.pi / 4), 0.0],
-    [0., 0., 1.]])
+    [np.cos(angle), np.sin(angle), 0.0],
+    [-np.sin(angle), np.cos(angle), 0.0],
+    [0.001, 0., 1.]])
+# snuck in a little perspective here.
 
 # ...around its center
+tr = image.shape[1] / 2, image.shape[0] / 2
 translation = np.matrix([
-    [1.0, 0.0, image.shape[1] / 2],
-    [0.0, 1.0, image.shape[0] / 2],
+    [1.0, 0.0, tr[0]],
+    [0.0, 1.0, tr[1]],
     [0.0, 0.0, 1.0]])
 
 # Compose the two previous matrices and warp the image
@@ -127,88 +131,34 @@ roi2.addScaleHandle([0, 0], [1, 1])
 roi1.addScaleRotateHandle([1, 0], [1, 1])
 roi2.addScaleRotateHandle([1, 0], [1, 1])
 
+# Convert OpenCV-style transform to Qt-style.
+# First, swap x <=> y
+qtmat = warpmat[[1,0,2]][:,[1,0,2]]
+# Transpose and build QTransform:
+elements = np.array(qtmat).T.flatten()
+roitrans = pg.QtGui.QTransform(*elements)
+transitem = pg.ItemGroup()
+transitem.setTransform(roitrans)
+roi2.setParentItem(transitem)
+
 vb1.addItem(roi1)
-vb2.addItem(roi2)
+vb2.addItem(transitem)
+
+
 
 
 # This method will handle ROI region changes
 def regionChanged(source):
     """This method will handle ROI region changes."""
 
-    # In the following comments, the "source" will refer to the image whose ROI
-    # was just changed by the user, and the "target" will refer to the other
-    # image, whose ROI we now have to update so that both ROI show the same
-    # image region.
-    #
-    # The (O), (OX) and (OY) notations will refer to the source or target ROI's
-    # axes in their own coordinate systems.
-
-    # Find the target ROI and retrieve the associated image items and
-    # transformation matrix.
+    # 'source' refers to the ROI that was moved by the user; 
+    # 'target' will be the other ROI.
     target = roi2 if source == roi1 else roi1
-    source_image = im1 if source == roi1 else im2
-    target_image = im2 if source == roi1 else im1
-    transform = warpmat if source == roi1 else invwarp
-
-    # Make a list of points representing the source ROI
-    sx, sy = source.size()
-    source_points = [
-        QtCore.QPointF(0.0, 0.0),   # (O)rigin of the source ROI's coordinates
-        QtCore.QPointF(sx, 0.0),    # (OX) axis
-        QtCore.QPointF(0.0, sy)]    # (OY) axis
-
-    # These coordinates were in the source ROI's coordinate system (i.e.
-    # rotated, scaled and translated wrt. the source image's origin). We now
-    # need to convert them to the coordinate system of the source image.
-    target_points = [source.mapToItem(source_image, h) for h in source_points]
-
-    # Transform those points using the perspective transformation we defined
-    # earlier to warp the first image into the second image.
-    target_points = [np.array([p.y(), p.x(), 1.0]) for p in target_points]
-    target_points = [p * transform.T for p in target_points]
-
-    # Now that those points have been transformed, we need to transform them
-    # back to the target image's coordinate system.
-    target_points = [np.array(p).ravel() for p in target_points]
-    target_points = [QtCore.QPointF(y / w, x / w) for x, y, w in target_points]
-
-    # Using those transformed points, we are going to determine the position,
-    # rotation and scale of the target ROI. Note that since both the target ROI
-    # and the target image are children of the same ViewBox, the position of
-    # the ROI is already defined in the target image's coordinate system.
-
-    # The position (i.e. the origin) of the ROI is easy to determine : it's the
-    # transformed position of the source origin.
-    position = target_points[0]
-
-    # We can also determine the target (OX) and (OY) vectors, thus defining the
-    # X and Y axes for the target ROI.
-    ux = target_points[1] - target_points[0]
-    uy = target_points[2] - target_points[0]
-
-    # Their norm gives us the size of the target ROI.
-    ux = np.array([ux.x(), ux.y()])
-    uy = np.array([uy.x(), uy.y()])
-    sizex = np.linalg.norm(ux)
-    sizey = np.linalg.norm(uy)
-
-    # The rotation angle of the target ROI is the rotation angle of the (OX)
-    # axis wrt. an horizontal line in the image's coordinate system.
-    u0 = np.array([1.0, 0.0])
-    rcos = np.dot(u0, ux / sizex)
-    rsin = np.cross(u0, ux / sizex)
-    rotation = math.acos(rcos)
-    rotation = -rotation if rsin < 0 else rotation
-    rotation = 180.0 * rotation / math.pi
-
-    # Now that we have everything we need, we can Update the target ROI The
-    # 'finish' and 'udpate' arguments are set to False to avoid emitting
-    # other signals when modifying the target ROI.
+    
+    # Copy the ROI state from source to target.
     try:
         target.sigRegionChanged.disconnect(regionChanged)
-        target.setRotation(rotation)
-        target.setSize((sizex, sizey), finish=False, update=False)
-        target.setPos(position)
+        target.setState(source.saveState())
     finally:
         target.sigRegionChanged.connect(regionChanged)
         
@@ -226,4 +176,4 @@ regionChanged(roi1)
 
 win.showMaximized()
 win.setWindowTitle("PyQtGraph Examples - Linked ROIs")
-app.exec_()
+#app.exec_()
